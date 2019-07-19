d37dcd997bac27f9319de7c6437fd8695267ac60
Indra Kusuma <indrakusuma.udinus@gmail.com>
2019-07-19 14:54:01 +0700

    feat: add example of module rules

--- webpack.config.js
+++ webpack.config.js
@@ -7,4 +7,16 @@ module.exports = {
     path: path.join(__dirname, 'dist'), // because use relative path, so we need path from node modules
     filename: 'app.bundle.js',
   },
+  module: {
+    rules: [
+      {
+        test: /\.js$/, // regex expression for get all js file
+        loader: 'babel-loader',
+        exclude: /node_modules/, // node_modules should be exclude because we dont want to compile NODE_MODULES
+        options: {
+          presets: ['@babel/preset-env'], // we need our code can be run on old browser, so we need babel for transform our code to old javascript syntax
+        },
+      },
+    ],
+  },
 };
