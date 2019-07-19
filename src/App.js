5fd90e1ad4c61db8bea08eb64974264c36227a1e
Indra Kusuma <indrakusuma.udinus@gmail.com>
2019-07-19 15:30:23 +0700

    feat: add webpack-dev-server and config

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 function App() {
-  return <h1>Hello Kusuma!</h1>
+  return <h1>Hello Kusuma y!</h1>
 }
 
 export default App;
--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -3,4 +3,7 @@ const baseConfig = require('./webpack.config.base');
 
 module.exports = merge(baseConfig, {
   mode: 'development',
+  devServer: {
+    port: 4000, // you can custom to port whatever you want
+  },
 });
