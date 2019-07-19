6edcd86a1ac1e87e09f876b2ac939470c0a50161
Indra Kusuma <indrakusuma.udinus@gmail.com>
2019-07-19 17:22:44 +0700

    feat: add support bundle analyzer after build

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,6 +1,16 @@
 const merge = require('webpack-merge');
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 const baseConfig = require('./webpack.config.base');
 
 module.exports = merge(baseConfig, {
   mode: 'production',
+  plugins: [
+    new BundleAnalyzerPlugin({
+      analyzerMode: 'static',
+      reportFilename: 'bundle_size.html',
+      openAnalyzer: false,
+    }),
+  ],
+  // for more details of BundleAnalyzer, 
+  // check this out https://github.com/webpack-contrib/webpack-bundle-analyzer
 });
