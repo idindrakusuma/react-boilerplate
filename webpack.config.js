ca01ec1e4c88610f3fbd7c78663041b336420c5a
Indra Kusuma <indrakusuma.udinus@gmail.com>
2019-07-19 15:05:51 +0700

    feat: add @babel/preset-react for compile our JSX code

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,24 @@
 const path = require('path');
 
 module.exports = {
-  mode: 'production',
-  entry: './src/index.js',
+  mode: "production",
+  entry: "./src/index.js",
   output: {
-    path: path.join(__dirname, 'dist'), // because use relative path, so we need path from node modules
-    filename: 'app.bundle.js',
+    path: path.join(__dirname, "dist"), // because use relative path, so we need path from node modules
+    filename: "app.bundle.js"
   },
   module: {
     rules: [
       {
-        test: /\.js$/, // regex expression for get all js file
-        loader: 'babel-loader',
+        test: /\.js$/, // regex expression for get all `.js` file
+        loader: "babel-loader",
         exclude: /node_modules/, // node_modules should be exclude because we dont want to compile NODE_MODULES
         options: {
-          presets: ['@babel/preset-env'], // we need our code can be run on old browser, so we need babel for transform our code to old javascript syntax
-        },
-      },
-    ],
-  },
+          presets: ["@babel/preset-env", "@babel/preset-react"]
+          // so we need `@babel/preset-env` for transform our code to old javascript syntax.
+          // and we need `@babel/preset-react` for compile our JSX code to old javascript syntax
+        }
+      }
+    ]
+  }
 };
