ab42094a81b69f15c1956b6ec0898076952955c9
Indra Kusuma <indrakusuma.udinus@gmail.com>
2019-07-20 19:05:58 +0700

    fix: make global jest runner

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  setupFilesAfterEnv: ['@testing-library/react/cleanup-after-each'],
+  setupFilesAfterEnv: ['@testing-library/react/cleanup-after-each', '@testing-library/jest-dom/extend-expect'],
   transform: {
     '^.+\\.js$': 'babel-jest',
     '.+\\.(css|styl|less|sass|scss)$': 'jest-transform-css',
